abce1ef64177c9460f0d70c04987c4bf821558d3
Rafferty Uy <Rafferty.Uy+MSDEMO@microsoft.com>
2023-06-07 16:58:13 +0800

    updates

--- LangChain/orchestrator-chat-multisessions-inmemory/app.py
+++ LangChain/orchestrator-chat-multisessions-inmemory/app.py
@@ -99,5 +99,6 @@ def chat():
             "total_tokens": total_tokens
         })
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
